0deeb5a20b570653908764b37418f396ba61a92c
Phillsm <github@psmith.dk>
2014-10-30 08:16:36 -0400

    Setup a crude orders page, might need to refactor the database access

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var mongo = require('./database/mongo');
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var orders = require('./routes/orders');
 
 var app = express();
 
@@ -22,7 +23,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+mongo.connect();
+app.use('/', orders);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
