89587b1a2ca268557ce8c093b0bdf6dd1f209ae0
Phillsm <github@psmith.dk>
2014-10-31 05:58:54 -0400

    populated order employee and customer objects

--- routes/order.js
+++ routes/order.js
@@ -1,19 +1,42 @@
 var db = require('../database/model')
 /* GET home page. */
 
+
 function orderHandler (req,res){
-  db.OrderModel.findById(req.params.id,function(err,order){
+  db.OrderModel.findById(req.params.id)
+  .populate('customer')
+  .populate('employee')
+  .exec(function(err,order){
     db.DetailsModel.find({order: order._id})
     .populate('product')
     .exec(function(err,details){
       details.forEach(function(elem){
         elem.totalPrice = elem.unitPrice * elem.quantity
       })
-      res.render('order',{order : order, details : details})
+  //    console.log(order.customer)
+//      console.log(order.employee)
+      res.render('order',{order:order,details:details})
     })
   })
 }
 
+/*
+function orderHandler (req,res){
+  db.OrderModel.findById(req.params.id,function(err,order){
+    db.DetailsModel.find({order: order._id})
+    .populate('product')
+//    .populate('customers')
+//    .populate('employee')
+    .exec(function(err,details){
+      details.forEach(function(elem){
+        elem.totalPrice = elem.unitPrice * elem.quantity
+      })
+      console.log(details);
+      res.render('order',{order : order, details : details})
+    })
+  })
+}
+*/
 module.exports = orderHandler
 
 
